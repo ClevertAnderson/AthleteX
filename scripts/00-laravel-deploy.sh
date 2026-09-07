12adb77333c711acdc43ebd45d6f82de4e3aff3a
unknown <network230206@gmail.com>
2026-09-07 10:02:53 +0800

    Remove route:cache due to closure route incompatibility

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
+echo "Clearing old caches..."
+php artisan config:clear
+php artisan route:clear
 echo "Caching config..."
 php artisan config:cache
-echo "Caching routes..."
-php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
